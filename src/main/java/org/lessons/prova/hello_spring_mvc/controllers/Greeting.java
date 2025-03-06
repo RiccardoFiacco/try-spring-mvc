92029003bc78d78537901f6e7ef29d818bd7ec6b
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-03-06 18:21:17 +0100

    removed an import

--- src/main/java/org/lessons/prova/hello_spring_mvc/controllers/Greeting.java
+++ src/main/java/org/lessons/prova/hello_spring_mvc/controllers/Greeting.java
@@ -1,8 +1,6 @@
 package org.lessons.prova.hello_spring_mvc.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
